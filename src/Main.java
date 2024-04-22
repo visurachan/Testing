07e65ebc19b164047d4204df24bbdf183aefe4de
visurachan <visurachandula@gmail.com>
2024-04-22 10:26:51 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[] args){
+        int x;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input value for x:");
+        x = scanner.nextInt();
+
+        if (x==5){
+            System.out.println("x is 5");
+        }
+        else {
+            System.out.println("x is not 5");
+        }
+        System.out.println("This prints no matter what");
+    }
+}
